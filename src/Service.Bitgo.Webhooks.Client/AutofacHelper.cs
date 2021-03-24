966b9146a46fb45ffacc5f39abeffd5a5a5f7c2b
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-03-24 13:20:04 +0200

    update nuget's

---
+++ src/Service.Bitgo.Webhooks.Client/AutofacHelper.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using DotNetCoreDecorators;
+using MyServiceBus.Abstractions;
+using MyServiceBus.TcpClient;
+using Service.Bitgo.Webhooks.Domain.Models;
+// ReSharper disable UnusedMember.Global
+
+namespace Service.Bitgo.Webhooks.Client
+{
+    public static class AutofacHelper
+    {
+        public static void RegisterSignalBitGoTransferSubscriber(this ContainerBuilder builder,
+            MyServiceBusTcpClient client,
+            string queueName,
+            TopicQueueType queryType)
+        {
+            var subs = new SignalBitGoTransferSubscriber(client, queueName, queryType);
+
+            builder
+                .RegisterInstance(subs)
+                .As<ISubscriber<SignalBitGoTransfer>>()
+                .SingleInstance();
+        }
+    }
+}
